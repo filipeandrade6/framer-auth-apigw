cd6f387f9efa136934423697cd87dfd4e1f781f2
Filipe Andrade <filipe.engenharia42@gmail.com>
2025-02-01 16:40:29 -0300

    trying to fix POST lambda api gateway integration

--- deployments/api-gw.tf
+++ deployments/api-gw.tf
@@ -30,7 +30,7 @@ resource "aws_api_gateway_resource" "upload" {
 resource "aws_api_gateway_method" "upload" {
   rest_api_id = aws_api_gateway_rest_api.fiap44.id
   resource_id = aws_api_gateway_resource.upload.id
-  http_method = "GET"
+  http_method = "ANY"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = aws_api_gateway_authorizer.fiap44.id
 }
