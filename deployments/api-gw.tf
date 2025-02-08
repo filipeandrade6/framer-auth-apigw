7cbeae69754a2b4dded391f412564df79f7c8073
Filipe Andrade <69737730+filipeandrade6@users.noreply.github.com>
2025-02-08 20:02:34 -0300

    added new api-gw resources

--- deployments/api-gw.tf
+++ deployments/api-gw.tf
@@ -17,6 +17,11 @@ resource "aws_api_gateway_authorizer" "fiap44" {
   provider_arns = [aws_cognito_user_pool.fiap44.arn]
 }
 
+resource "aws_api_gateway_vpc_link" "fiap44-vpc-link" {
+  name        = "fiap44-vpc-link"
+  target_arns = [ "${data.aws_lb.fiap44-alb.arn}" ]
+}
+
 #####
 ## upload (lambda)
 #####
@@ -110,6 +115,169 @@ resource "aws_api_gateway_integration_response" "download" {
 }
 
 #####
+## videos
+#####
+
+## aws_api_gateway_resource
+resource "aws_api_gateway_resource" "videos" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  parent_id = aws_api_gateway_rest_api.fiap44.root_resource_id
+  path_part = "videos"
+}
+
+resource "aws_api_gateway_resource" "videos_id" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  parent_id = aws_api_gateway_resource.videos.id
+  path_part = "{id}"
+  
+}
+
+resource "aws_api_gateway_resource" "videos_email" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  parent_id   = aws_api_gateway_resource.videos.id
+  path_part   = "email"
+}
+
+resource "aws_api_gateway_resource" "videos_email_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  parent_id   = aws_api_gateway_resource.videos_email.id
+  path_part   = "{email}"
+}
+
+resource "aws_api_gateway_method" "videos_list" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos.id
+  http_method = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.fiap44.id
+}
+
+resource "aws_api_gateway_method" "videos_email_get" {
+  rest_api_id   = aws_api_gateway_rest_api.fiap44.id
+  resource_id   = aws_api_gateway_resource.videos_email_get.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.fiap44.id
+
+  request_parameters = {
+    "method.request.path.email" = true
+  }
+}
+
+resource "aws_api_gateway_method" "videos_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos_id.id
+  http_method = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.fiap44.id
+
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+## aws_api_gateway_integration
+resource "aws_api_gateway_integration" "videos_list" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos.id
+  http_method = aws_api_gateway_method.videos_list.http_method
+  integration_http_method = "GET"
+  type = "HTTP"
+  connection_type = "VPC_LINK"
+  connection_id = aws_api_gateway_vpc_link.fiap44-vpc-link.id
+  uri = "http://${data.aws_lb.fiap44-alb.dns_name}/videos"
+}
+
+resource "aws_api_gateway_integration" "videos_email_get" {
+  rest_api_id             = aws_api_gateway_rest_api.fiap44.id
+  resource_id             = aws_api_gateway_resource.videos_email_get.id
+  http_method             = aws_api_gateway_method.videos_email_get.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP"
+  connection_type         = "VPC_LINK"
+  connection_id           = aws_api_gateway_vpc_link.fiap44-vpc-link.id
+  uri                     = "http://${data.aws_lb.fiap44-alb.dns_name}/videos/email/{email}"
+
+  request_parameters = {
+    "integration.request.path.email" = "method.request.path.email"
+  }
+}
+
+resource "aws_api_gateway_integration" "videos_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos_id.id
+  http_method = aws_api_gateway_method.videos_get.http_method
+  integration_http_method = "GET"
+  type = "HTTP"
+  connection_type = "VPC_LINK"
+  connection_id = aws_api_gateway_vpc_link.fiap44-vpc-link.id
+  uri = "http://${data.aws_lb.fiap44-alb.dns_name}/videos/{id}"
+
+  request_parameters = {
+    "integration.request.path.id" = "method.request.path.id"
+  }
+}
+
+## aws_api_gateway_method_response
+resource "aws_api_gateway_method_response" "videos_list" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos.id
+  http_method = aws_api_gateway_method.videos_list.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_method_response" "videos_email_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos_email_get.id
+  http_method = aws_api_gateway_method.videos_email_get.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_method_response" "videos_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos_id.id
+  http_method = aws_api_gateway_method.videos_get.http_method
+  status_code = "200"
+}
+
+## aws_api_gateway_integration_response
+resource "aws_api_gateway_integration_response" "videos_list" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos.id
+  http_method = aws_api_gateway_method.videos_list.http_method
+  status_code = aws_api_gateway_method_response.videos_list.status_code
+
+  depends_on = [
+    aws_api_gateway_method.videos_list,
+    aws_api_gateway_integration.videos_list,
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "videos_email_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos_email_get.id
+  http_method = aws_api_gateway_method.videos_email_get.http_method
+  status_code = aws_api_gateway_method_response.videos_email_get.status_code
+
+  depends_on = [
+    aws_api_gateway_method.videos_email_get,
+    aws_api_gateway_integration.videos_email_get,
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "videos_get" {
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
+  resource_id = aws_api_gateway_resource.videos_id.id
+  http_method = aws_api_gateway_method.videos_get.http_method
+  status_code = aws_api_gateway_method_response.videos_get.status_code
+
+  depends_on = [
+    aws_api_gateway_method.videos_get,
+    aws_api_gateway_integration.videos_get,
+  ]
+}
+
+#####
 ## api gw deploy
 #####
 
@@ -117,6 +285,9 @@ resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.upload,
     aws_api_gateway_integration.download,
+    aws_api_gateway_integration.videos_email_get,
+    aws_api_gateway_integration.videos_get,
+    aws_api_gateway_integration.videos_list
   ]
 
   rest_api_id = aws_api_gateway_rest_api.fiap44.id
@@ -151,4 +322,10 @@ data "aws_lambda_function" "fiap44_framer_psgr_upload" {
 
 data "aws_lambda_function" "fiap44_framer_psgr_download" {
   function_name = "fiap44_framer_psgr_download"
+}
+
+data "aws_lb" "fiap44-alb" {
+  tags = {
+    "kubernetes.io/service-name" = "default/ingress-nginx-controller"
+  }
 }
